c90b93556f67323f263a94fb17637f801781ad6d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-11 01:34:39 +0300

    fix: remove follow inputs so that flakes that use this flake work properly

--- flake.nix
+++ flake.nix
@@ -2,15 +2,9 @@
   inputs = {
     devshell.url = "github:numtide/devshell";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk = {
-      url = "github:nmattia/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    naersk.url = "github:nmattia/naersk";
     flakeUtils.url = "github:numtide/flake-utils";
-    rustOverlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rustOverlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs: with inputs;
