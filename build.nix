dd9edfb43461f2d6a7fe9d917447259ef12560e0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-15 01:42:46 +0300

    feat: support specificing path to desktop file, cleanup desktop file attrs

--- build.nix
+++ build.nix
@@ -7,8 +7,7 @@
 let
   inherit (common) pkgs packageMetadata cargoPkg;
 
-  xdgMetadata = packageMetadata.xdg or null;
-  makeDesktopFile = xdgMetadata.enable or false;
+  desktopFileMetadata = packageMetadata.desktopFile or null;
 
   cargoLicenseToNixpkgs = license:
     let
@@ -19,8 +18,10 @@ let
         "gplv2" = "gpl2";
       }."${l}" or l;
 
+  # TODO convert cargo maintainers to nixpkgs maintainers
   meta = with pkgs.lib; ({
     description = cargoPkg.description or "${cargoPkg.name} is a Rust project.";
+    platforms = [ common.system ];
   } // (optionalAttrs (builtins.hasAttr "license" cargoPkg) { license = licenses."${cargoLicenseToNixpkgs cargoPkg.license}"; })
   // (optionalAttrs (builtins.hasAttr "homepage" cargoPkg) { inherit (cargoPkg) homepage; })
   // (optionalAttrs (builtins.hasAttr "longDescription" packageMetadata) { inherit (packageMetadata) longDescription; }));
@@ -33,15 +34,23 @@ let
         if (hasPrefix "./" icon)
         then (common.root + "/${removePrefix "./" icon}")
         else icon;
+      desktopFilePath = common.root + "/${removePrefix "./" desktopFileMetadata}";
     in
-    ((pkgs.makeDesktopItem {
-      inherit name;
-      exec = packageMetadata.executable or name;
-      comment = xdgMetadata.comment or meta.description;
-      desktopName = xdgMetadata.name or name;
-    }) // (optionalAttrs (builtins.hasAttr "icon" xdgMetadata) { icon = makeIcon xdgMetadata.icon; })
-    // (optionalAttrs (builtins.hasAttr "genericName" xdgMetadata) { inherit (xdgMetadata) genericName; })
-    // (optionalAttrs (builtins.hasAttr "categories" xdgMetadata) { inherit (xdgMetadata) categories; }));
+    if builtins.isString desktopFileMetadata
+    then
+      pkgs.runCommand "${cargoPkg.name}-desktopFileLink" { } ''
+        mkdir -p $out/share/applications
+        ln -sf ${desktopFilePath} $out/share/applications
+      ''
+    else
+      ((pkgs.makeDesktopItem {
+        inherit name;
+        exec = packageMetadata.executable or name;
+        comment = desktopFileMetadata.comment or meta.description;
+        desktopName = desktopFileMetadata.name or name;
+      }) // (optionalAttrs (builtins.hasAttr "icon" desktopFileMetadata) { icon = makeIcon desktopFileMetadata.icon; })
+      // (optionalAttrs (builtins.hasAttr "genericName" desktopFileMetadata) { inherit (desktopFileMetadata) genericName; })
+      // (optionalAttrs (builtins.hasAttr "categories" desktopFileMetadata) { inherit (desktopFileMetadata) categories; }));
 
   package = with pkgs;
     let
@@ -54,7 +63,7 @@ let
         cargoTestOptions = def: def ++ [ "--tests" "--bins" "--examples" ] ++ (lib.optional library "--lib");
         override = (prev: common.env);
         overrideMain = (prev: common.env // { inherit meta; } // (
-          lib.optionalAttrs makeDesktopFile
+          lib.optionalAttrs (!(isNull desktopFileMetadata))
             { nativeBuildInputs = prev.nativeBuildInputs ++ [ copyDesktopItems ]; desktopItems = [ desktopFile ]; }
         ));
         copyLibs = library;
