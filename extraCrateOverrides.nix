04d8abb0395cffe3d2b6d02d076ad9a40aa1909e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-26 21:50:22 +0300

    feat: more def overrides for crates

--- extraCrateOverrides.nix
+++ extraCrateOverrides.nix
@@ -11,8 +11,25 @@ with pkgs;
   alsa-sys = mkOv [ alsaLib ] [ pkg-config ];
   xcb = mkOv [ xorg.libxcb ] [ python3 ];
   xkbcommon-sys = mkOv [ libxkbcommon ] [ pkg-config ];
+  expat-sys = mkOv [ expat ] [ cmake ];
+  freetype-sys = mkOv [ freetype ] [ cmake ];
+  servo-fontconfig-sys = mkOv [ fontconfig ] [ pkg-config ];
+  cairo-sys-rs = mkOv [ cairo ] [ pkg-config ];
+  pango-sys = mkOv [ pango ] [ pkg-config ];
+  glib-sys = mkOv [ glib ] [ pkg-config ];
+  gobject-sys = mkOv [ glib ] [ pkg-config ];
+  gio-sys = mkOv [ glib ] [ pkg-config ];
+  atk-sys = mkOv [ atk ] [ pkg-config ];
+  gdk-pixbuf-sys = mkOv [ gdk_pixbuf ] [ pkg-config ];
+  gdk-sys = mkOv [ gtk3 ] [ pkg-config ];
+  gtk-sys = mkOv [ gtk3 ] [ pkg-config ];
   harmony_rust_sdk = prev:
-    let env = { PROTOC = "protoc"; }; in
+    let
+      env = {
+        PROTOC = "${protobuf}/bin/protoc";
+        PROTOC_INCLUDE = "${protobuf}/include";
+      };
+    in
     {
       buildInputs = (prev.buildInputs or [ ]) ++ [ protobuf ];
       propagatedEnv = env;
