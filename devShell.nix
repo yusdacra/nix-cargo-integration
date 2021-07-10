5c265683f56bdae0e3c95b2ac5165810f55b3dda
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-07-10 16:45:57 +0200

    Push up cargo metadata 'nix.devshell.*' options
    to 'nix.devshell.devshell'.
    
    devshellOptions = { foo = ...; bar = ...; };
    pushUpDevshellOptions { foo = "foo"; baz = "baz"; }
    -> { devhsell.foo = "foo"; baz = "baz"; }
    
    This avoids writing 'devshell' twice when configuring it from
    `Cargo.toml`.

--- devShell.nix
+++ devShell.nix
@@ -7,9 +7,61 @@ let
   cachixName = cachixMetadata.name or null;
   cachixKey = cachixMetadata.key or null;
 
+  # Get all the options' name declared immediately under `config.devshell` by
+  # devshell's modules.
+  devshellOptions = lib.filterAttrs
+    (_: lib.isType "option")
+    (pkgs.devshell.eval { configuration = {}; }).options.devshell;
+
+  # A helper function moving all options defined in the root of the config
+  # (which name matches ones in `devshellOptions`) under a `devshell` attribute
+  # set in the resulting config.
+  #
+  # devshellOptions = { foo = ...; bar = ...; };
+  # pushUpDevshellOptions { foo = "foo"; baz = "baz"; }
+  # -> { devhsell.foo = "foo"; baz = "baz"; }
+  #
+  # Issues a warning if it would override an exisiting option:
+  #
+  # pushUpDevshellOptions { foo = "foo"; devshell.foo = "oof"; }
+  # -> { devhsell.foo = "foo"; }
+  # trace: warning: Option 'foo' defined twice, both under 'config' and
+  #   'config.devshell'. This likely happens when defining both in `Cargo.toml`:
+  #   ```toml
+  #   [workspace.metadata.nix.devshell]
+  #   name = "example"
+  #   [workspace.metadata.nix.devshell.devshell]
+  #   name = "example"
+  #   ```
+  pushUpDevshellOptions = config: let
+    movedOpts = lib.flip lib.filterAttrs config (name: _:
+      lib.warnIf
+        (lib.hasAttr name config.devshell)
+        (lib.concatStrings [
+          "Option '${name}' defined twice, both under 'config' and "
+          "'config.devshell'. This likely happens when defining both in "
+          ''
+            `Cargo.toml`:
+            ```toml
+            [workspace.metadata.nix.devshell]
+            name = "example"
+            [workspace.metadata.nix.devshell.devshell]
+            name = "example"
+            ```
+          ''
+        ])
+        (lib.hasAttr name devshellOptions)
+    );
+  in lib.recursiveUpdate
+    (builtins.removeAttrs config (lib.attrNames movedOpts))
+    { devshell = movedOpts; };
+
   # Make devshell configs
   devshellAttr = workspaceMetadata.devshell or packageMetadata.devshell or null;
-  devshellConfig = if pkgs.lib.isAttrs devshellAttr then (builtins.removeAttrs devshellAttr [ "imports" ]) else { };
+  devshellConfig =
+    if pkgs.lib.isAttrs devshellAttr then
+      pushUpDevshellOptions (builtins.removeAttrs devshellAttr [ "imports" ])
+    else { };
   devshellFilePath = common.prevRoot + "/devshell.toml";
 
   # Import the devshell specified in devshell.toml if it exists
