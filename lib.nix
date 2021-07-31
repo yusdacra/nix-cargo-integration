79e089df7804215471271ae3f19ea1e85190c8f4
Ben Merritt <blm768@gmail.com>
2021-07-31 00:25:06 -0700

    Warn if no packages are found

--- lib.nix
+++ lib.nix
@@ -262,6 +262,10 @@ in
       } // lib.optionalAttrs (builtins.hasAttr "app" defaultOutputs) {
         defaultApp = lib.mapAttrs (_: system: system.${defaultOutputs.app}) combinedOutputs.apps;
       };
+      checkedOutputs = lib.warnIf
+        (!(builtins.hasAttr "packages" finalOutputs) && !(builtins.hasAttr "apps" finalOutputs))
+        "No packages found. Did you add the package.metadata.nix section to Cargo.toml?"
+        finalOutputs;
     in
-    finalOutputs;
+    checkedOutputs;
 }
