9d0eb1670ffaccb216e7db1f28331b27cd7ac93f
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-03-06 17:23:36 +0300

    chore(deps): update flake deps

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     dream2nix = {
-      url = "github:nix-community/dream2nix/main";
+      url = "github:yusdacra/dream2nix/rust/crane-fixes";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.gomod2nix.follows = "nixpkgs";
       inputs.mach-nix.follows = "nixpkgs";
