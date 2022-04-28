24b93785f15381f6f2f0f6d5a66e8a138e07c172
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-04-28 21:07:37 +0300

    fix: patch override from nixpkgs defaultCrateOverrides

--- src/extra-crate-overrides.nix
+++ src/extra-crate-overrides.nix
@@ -7,6 +7,7 @@
     buildInputs = (prev.buildInputs or []) ++ bi;
     nativeBuildInputs = (prev.nativeBuildInputs or []) ++ ni;
   };
+
   ffmpeg-sys-next = prev: let
     inherit (pkgs) ffmpeg llvmPackages pkg-config;
     inherit (llvmPackages) libclang;
@@ -19,51 +20,65 @@
       propagatedEnv = env;
     }
     // env;
-in {
-  inherit ffmpeg-sys-next;
-  ffmpeg-sys = ffmpeg-sys-next;
-  libudev-sys = with pkgs; mkOv [udev] [pkg-config];
-  alsa-sys = with pkgs; mkOv [alsa-lib] [pkg-config];
-  xcb = with pkgs; mkOv [xorg.libxcb] [python3];
-  xkbcommon-sys = with pkgs; mkOv [libxkbcommon] [pkg-config];
-  expat-sys = with pkgs; mkOv [expat] [cmake];
-  freetype-sys = with pkgs; mkOv [freetype] [cmake];
-  servo-fontconfig-sys = with pkgs; mkOv [fontconfig] [pkg-config];
-  cairo-sys-rs = with pkgs; mkOv [cairo] [pkg-config];
-  pango-sys = with pkgs; mkOv [pango harfbuzz] [pkg-config];
-  glib-sys = with pkgs; mkOv [glib] [pkg-config];
-  gobject-sys = with pkgs; mkOv [glib] [pkg-config];
-  gio-sys = with pkgs; mkOv [glib] [pkg-config];
-  atk-sys = with pkgs; mkOv [atk] [pkg-config];
-  gdk-pixbuf-sys = with pkgs; mkOv [gdk_pixbuf] [pkg-config];
-  gdk-sys = with pkgs; mkOv [gtk3] [pkg-config];
-  gtk-sys = with pkgs; mkOv [gtk3] [pkg-config];
-  harmony_rust_sdk = prev: let
-    inherit (pkgs) protobuf;
-    env = {
-      PROTOC = "${protobuf}/bin/protoc";
-      PROTOC_INCLUDE = "${protobuf}/include";
+
+  overrides = {
+    inherit ffmpeg-sys-next;
+    ffmpeg-sys = ffmpeg-sys-next;
+    libudev-sys = with pkgs; mkOv [udev] [pkg-config];
+    alsa-sys = with pkgs; mkOv [alsa-lib] [pkg-config];
+    xcb = with pkgs; mkOv [xorg.libxcb] [python3];
+    xkbcommon-sys = with pkgs; mkOv [libxkbcommon] [pkg-config];
+    expat-sys = with pkgs; mkOv [expat] [cmake];
+    freetype-sys = with pkgs; mkOv [freetype] [cmake];
+    servo-fontconfig-sys = with pkgs; mkOv [fontconfig] [pkg-config];
+    cairo-sys-rs = with pkgs; mkOv [cairo] [pkg-config];
+    pango-sys = with pkgs; mkOv [pango harfbuzz] [pkg-config];
+    glib-sys = with pkgs; mkOv [glib] [pkg-config];
+    gobject-sys = with pkgs; mkOv [glib] [pkg-config];
+    gio-sys = with pkgs; mkOv [glib] [pkg-config];
+    atk-sys = with pkgs; mkOv [atk] [pkg-config];
+    gdk-pixbuf-sys = with pkgs; mkOv [gdk_pixbuf] [pkg-config];
+    gdk-sys = with pkgs; mkOv [gtk3] [pkg-config];
+    gtk-sys = with pkgs; mkOv [gtk3] [pkg-config];
+    harmony_rust_sdk = prev: let
+      inherit (pkgs) protobuf;
+      env = {
+        PROTOC = "${protobuf}/bin/protoc";
+        PROTOC_INCLUDE = "${protobuf}/include";
+      };
+    in
+      {
+        buildInputs = [protobuf pkgsWithRust.rustfmt];
+        propagatedEnv = env;
+      }
+      // env;
+    shaderc-sys = _: let
+      env = {SHADERC_LIB_DIR = "${pkgs.shaderc.lib}/lib";};
+    in
+      {propagatedEnv = env;} // env;
+    prost-build = prev: let
+      inherit (pkgs) protobuf;
+      env = {
+        PROTOC = "${protobuf}/bin/protoc";
+        PROTOC_INCLUDE = "${protobuf}/include";
+      };
+    in
+      {
+        buildInputs = [protobuf pkgsWithRust.rustfmt];
+        propagatedEnv = env;
+      }
+      // env;
+    security-framework-sys = prev: {
+      propagatedBuildInputs =
+        l.optional
+        pkgs.stdenv.isDarwin
+        pkgs.darwin.apple_sdk.frameworks.Security;
     };
-  in
-    {
-      buildInputs = [protobuf pkgsWithRust.rustfmt];
-      propagatedEnv = env;
-    }
-    // env;
-  shaderc-sys = _: let
-    env = {SHADERC_LIB_DIR = "${pkgs.shaderc.lib}/lib";};
-  in
-    {propagatedEnv = env;} // env;
-  prost-build = prev: let
-    inherit (pkgs) protobuf;
-    env = {
-      PROTOC = "${protobuf}/bin/protoc";
-      PROTOC_INCLUDE = "${protobuf}/include";
-    };
-  in
-    {
-      buildInputs = [protobuf pkgsWithRust.rustfmt];
-      propagatedEnv = env;
-    }
-    // env;
-}
+  };
+in
+  overrides
+  // (
+    l.filterAttrs
+    (n: _: n != "security-framework-sys")
+    pkgs.defaultCrateOverrides
+  )
