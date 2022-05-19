824ce81009baf3e87c786113f4cbdc966120ac8e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-05-19 19:22:33 +0300

    docs: update example flake

--- docs/example_flake.nix
+++ docs/example_flake.nix
@@ -10,6 +10,8 @@
       # Usually you don't need mess with this.
       # The default is "crane".
       builder = "crane";
+      # Change the systems to generate outputs for.
+      # systems = ["x86_64-linux"];
       # Which package outputs to rename to what.
       # This renames both their package names and the generated output names.
       # Applies to generated apps too.
@@ -21,15 +23,13 @@
         # Set the `defaultApp` output to the "example` app from `apps`.
         # app = "example";
       };
+      # Overlays to use for the nixpkgs package set.
+      pkgsOverlays = [];
       # Overrides provided here will apply to *every crate*,
       # for *every system*. To selectively override per crate,
       # one can use `common.cargoPkg.name` attribute. To selectively
       # override per system one can use `common.system` attribute.
       overrides = {
-        # Override the systems to generate outputs for.
-        systems = prev: prev;
-        # Overlays to use for the nixpkgs package set.
-        pkgsOverlays = [];
         # Override crate overrides.
         #
         # The environment variables and build inputs specified here will
@@ -59,6 +59,12 @@
               # lib.nameValuePair "PROTOC" "protoc"
               # { name = "HOME_DIR"; eval = "$HOME"; }
             ];
+        };
+        # Override the build configuration.
+        # This corresponds to `dream2nix`'s `makeFlakeOutputs` arguments.
+        build = common: prev: {
+          # for example, set the translator to something else
+          # settings = (prev.settings or []) ++ [{translator = "cargo-toml";}];
         };
       };
     };
