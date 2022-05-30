bf8b42eef73b95a9f5faa53ae83c9ed1cb5fb3d8
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-05-31 00:01:45 +0300

    fix: disable dream2nix ifd warning

--- src/pkgs-set.nix
+++ src/pkgs-set.nix
@@ -73,6 +73,7 @@ in rec {
   # dream2nix tools
   dream2nix = sources.dream2nix.lib.init {
     config.projectRoot = root;
+    config.disableIfdWarning = true;
     pkgs = pkgsWithRust;
   };
   # devshell
