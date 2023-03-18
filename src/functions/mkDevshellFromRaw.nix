23ccce67d870af937bcfbb08c4a6e42cec82cb4c
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-03-19 02:48:14 +0300

    fix: make devshell overrideAttrs process packages argument (#116)

--- src/functions/mkDevshellFromRaw.nix
+++ src/functions/mkDevshellFromRaw.nix
@@ -8,11 +8,30 @@
   runtimeLibsEnv = l.optionalAttrs (l.length runtimeLibs > 0) {
     LD_LIBRARY_PATH = "${l.makeLibraryPath runtimeLibs}";
   };
-in
-  rawShell.overrideAttrs (old:
+  base = rawShell.overrideAttrs (old:
     runtimeLibsEnv
     // {
       nativeBuildInputs =
         [(lib.hiPrio shellToolchain)]
         ++ (old.nativeBuildInputs or []);
-    })
+    });
+  overrideAttrs = shell: f: let
+    new = base.overrideAttrs (
+      old: let
+        attrs = f old;
+      in
+        attrs
+        // {
+          nativeBuildInputs =
+            (attrs.packages or [])
+            ++ (attrs.nativeBuildInputs or [])
+            ++ (old.nativeBuildInputs or []);
+        }
+    );
+  in
+    new
+    // {
+      overrideAttrs = overrideAttrs new;
+    };
+in
+  overrideAttrs base (_: {})
