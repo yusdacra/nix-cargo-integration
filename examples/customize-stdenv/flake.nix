573cef4ffc0563d7f9e230cf11a974e249e252b9
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-03-30 18:11:39 +0300

    feat: add new example showcasing how to customize the stdenv

---
+++ examples/customize-stdenv/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+  outputs = inputs @ {
+    parts,
+    nci,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule];
+      perSystem = {pkgs, config, ...}: let
+        crateName = "example-crate";
+      in {
+        # declare projects
+        nci.projects.${crateName}.relPath = "";
+        # configure crates
+        nci.crates.${crateName} = let
+          # the override we'll use for setting the stdenv
+          set-stdenv = {
+            # in this case we will set it to the clang stdenv
+            override = {stdenv = pkgs.clangStdenv;};
+          };
+        in {
+          ### override stdenv for both dependencies and main derivation ###
+          depsOverrides = {
+            inherit set-stdenv;
+          };
+          overrides = {
+            inherit set-stdenv;
+          };
+        };
+      };
+    };
+}
--- examples/simple-crate/flake.nix
+++ examples/simple-crate/flake.nix
@@ -13,7 +13,7 @@
     parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       imports = [nci.flakeModule];
-      perSystem = {config, ...}: let
+      perSystem = {pkgs, config, ...}: let
         # TODO: change this to your crate's name
         crateName = "my-crate";
         # shorthand for accessing this crate's outputs
