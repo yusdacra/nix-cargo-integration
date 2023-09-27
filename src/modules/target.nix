78a30e74310cd39996284a64555a621a81e05ce0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-09-27 16:32:14 +0300

    refactor: remove features from targets and add description for profiles

--- src/modules/target.nix
+++ src/modules/target.nix
@@ -17,18 +17,8 @@ in {
       example = l.literalExpression ''
         ["dev" "release" "custom-profile"]
       '';
-    };
-    features = l.mkOption {
-      type = t.nullOr (t.listOf t.str);
-      default = null;
-      defaultText = ''["default"]'';
-      example = l.literalExpression ''
-        ["tracing" "publish"]
-      '';
       description = ''
-        Features to enable for this target. Set to 'null' to enable default features only (this is the default).
-        If set to a list of features then '--no-default-features' will be passed to Cargo.
-        If you want to also enable default features you can add 'default' feature to the list of features.
+        The profiles to generate packages for this target.
       '';
     };
   };
