7f4a7d7f18abdff8168bcf333e8ec5eccd08987a
dusk <y.bera003.06@protonmail.com>
2024-11-14 19:24:49 +0900

    refactor(examples): separate nci code from the flake.nix for numtide example

--- examples/numtide-devshell/flake.nix
+++ examples/numtide-devshell/flake.nix
@@ -13,32 +13,7 @@
       imports = [
         inputs.nci.flakeModule
         inputs.devshell.flakeModule
+        ./crates.nix
       ];
-
-      perSystem = {pkgs, ...}: {
-        # declare projects
-        nci.projects."my-project" = {
-          path = ./.;
-          # Configure the numtide devshell to which all packages
-          # required for this project and its crates should be added
-          numtideDevshell = "default";
-        };
-
-        # configure crates
-        nci.crates."my-crate" = {
-          # If you only want to add requirements for a specific
-          # crate to your numtide devshell:
-          #numtideDevshell = "default";
-          drvConfig.mkDerivation.buildInputs = [pkgs.hello];
-        };
-
-        # Conveniently configure additional things in your devshell
-        devshells.default.env = [
-          {
-            name = "SOME_EXTRA_ENV_VARIABLE";
-            value = "true";
-          }
-        ];
-      };
     };
 }
