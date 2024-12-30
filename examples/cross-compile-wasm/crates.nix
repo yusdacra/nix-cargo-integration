a0e99ac1e98e12b280e87239526cb7c3bac33dd7
dusk <y.bera003.06@protonmail.com>
2024-12-30 11:59:42 +0900

    fix: make sure wasm example gets correct version

--- examples/cross-compile-wasm/crates.nix
+++ examples/cross-compile-wasm/crates.nix
@@ -13,34 +13,17 @@
     nci.crates.${crateName} = {
       targets."wasm32-unknown-unknown" = {
         default = true;
+        drvConfig.env = {
+          TRUNK_TOOLS_SASS = pkgs.nodePackages.sass.version;
+          TRUNK_TOOLS_WASM_BINDGEN = pkgs.wasm-bindgen-cli.version;
+          TRUNK_TOOLS_WASM_OPT = "version_${lib.removeSuffix "_b" pkgs.binaryen.version}";
+          TRUNK_SKIP_VERSION_CHECK = "true";
+        };
         drvConfig.mkDerivation = {
           # add trunk and other dependencies
-          nativeBuildInputs =
-            (with pkgs; [nodePackages.sass wasm-bindgen-cli binaryen])
-            ++ [
-              (config.nci.lib.buildCrate rec {
-                src = pkgs.fetchFromGitHub {
-                  owner = "trunk-rs";
-                  repo = "trunk";
-                  rev = "v0.21.4";
-                  hash = "sha256-tU0Xob0dS1+rrfRVitwOe0K1AG05LHlGPHhFL0yOjxM=";
-                };
-                drvConfig = {
-                  mkDerivation = {
-                    nativeBuildInputs = [pkgs.pkg-config];
-                    buildInputs = [pkgs.openssl];
-                  };
-                  rust-crane.runTests = false;
-                };
-                depsDrvConfig.mkDerivation = drvConfig.mkDerivation;
-              })
-            ];
+          nativeBuildInputs = with pkgs; [trunk nodePackages.sass wasm-bindgen-cli binaryen];
           # override build phase to build with trunk instead
           buildPhase = ''
-            export TRUNK_TOOLS_SASS="${pkgs.nodePackages.sass.version}"
-            export TRUNK_TOOLS_WASM_BINDGEN="${pkgs.wasm-bindgen-cli.version}"
-            export TRUNK_TOOLS_WASM_OPT="version_${pkgs.binaryen.version}"
-            export TRUNK_SKIP_VERSION_CHECK="true"
             echo sass is version $TRUNK_TOOLS_SASS
             echo wasm bindgen is version $TRUNK_TOOLS_WASM_BINDGEN
             HOME=$TMPDIR \
--- examples/cross-compile-wasm/flake.nix
+++ examples/cross-compile-wasm/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nci.url = "path:/home/kirara/proj/nix-cargo-integration";
   inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
